fe4b6dc534bb47492a1fe1da0d45a4c25f5a840a
jvpink200 <jvillarr20@gmail.com>
2021-10-25 22:08:04 -0700

    added a style file and list basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React from 'react';
+
 function App() {
   return (
-    <div>Hello!</div>
+    <div>
+      <h1>My Todos</h1>
+      <div>
+        <h2>Title</h2>
+        <div className=''>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
