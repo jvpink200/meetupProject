5fc6e40061753e8c0250c1d602208a7f58a25ff7
jvpink200 <jvillarr20@gmail.com>
2021-10-26 21:23:07 -0700

    deleted todo modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import ToDo from './components/Todo';
+
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <ToDo title="title1" bio="something here 1" />
-      <ToDo title="title2" bio="something here 2" />
-      <ToDo title="title3" />
     </div>
   );
 }
--- src/components/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-//onClick is built in as it is not a custom component
-function BackDrop(props) {
-    return <div className='backdrop' onClick={props.onCancel}/>
-}
-
-export default BackDrop;
