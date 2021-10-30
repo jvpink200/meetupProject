3ca91f1b09e99311c59609510414207a670339bc
jvpink200 <jvillarr20@gmail.com>
2021-10-29 20:54:35 -0700

    added MainNavigation components and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import MainNavigation from './components/MainNavigation';
+   
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetUps";
+import NewMeetupPage from "./pages/NewMeetUp";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
       <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
